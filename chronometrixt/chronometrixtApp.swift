78f4eea15a0c6cc8fd7a3b3cf8272fee8fbb52a0
Becket <b@txixt.com>
2025-12-30 18:24:26 -0500

    Initial Commit

---
+++ chronometrixt/chronometrixtApp.swift
@@ -0,0 +1,32 @@
+//
+//  chronometrixtApp.swift
+//  chronometrixt
+//
+//  Created by Becket Bowes on 12/30/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct chronometrixtApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
